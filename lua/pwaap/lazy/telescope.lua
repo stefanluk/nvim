74633135c43d0db72b6c3f6fd0c7465818bdce71
Stefan Luk <sluk@anwb.nl>
2024-04-22 15:09:02 +0200

    Add telescope-ui-select

--- lua/pwaap/lazy/telescope.lua
+++ lua/pwaap/lazy/telescope.lua
@@ -1,20 +1,31 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
     },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
 
-    config = function()
-        require('telescope').setup({})
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-    end
+            local builtin = require("telescope.builtin")
+
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
-
