7d5b895706c9d9ac2b705674c1074218457562e0
Stefan Luk <sluk@anwb.nl>
2024-04-22 16:05:14 +0200

    add git keybindings for git commands

--- lua/pwaap/lazy/git.lua
+++ lua/pwaap/lazy/git.lua
@@ -8,8 +8,23 @@ local icons = {
 return {
     {
         "tpope/vim-fugitive",
-        config = function() 
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        config = function()
+            vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+            vim.keymap.set("n", "<leader>gp", function()
+                vim.cmd.Git('push')
+            end)
+            vim.keymap.set("n", "<leader>gl", function()
+                vim.cmd.Git('pull')
+            end)
+            vim.keymap.set("n", "<leader>gp", function()
+                vim.cmd.Git('push')
+            end)
+            vim.keymap.set("n", "<leader>gc", function()
+                vim.fn.input(":Git commit -m \"")
+            end)
+            vim.keymap.set("n", "<leader>gm", function()
+                vim.fn.input(":Git merge ")
+            end)
         end
     },
     {
@@ -48,28 +63,19 @@ return {
                         linehl = "GitSignsChangeLn",
                     },
                 },
-                -- signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-                -- numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-                -- linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-                -- word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-                -- watch_gitdir = {
-                --     follow_files = true
-                -- },
-                -- auto_attach = true,
+                signcolumn = true,
+                linehl = false,
                 attach_to_untracked = false,
-                -- current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                -- current_line_blame_opts = {
-                --     virt_text = true,
-                --     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                --     delay = 1000,
-                --     ignore_whitespace = false,
-                --     virt_text_priority = 100,
-                -- },
+                current_line_blame = true,
+                current_line_blame_opts = {
+                    virt_text = true,
+                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                    delay = 500,
+                    ignore_whitespace = false,
+                    virt_text_priority = 100,
+                },
                 current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-                -- current_line_blame_formatter_opts = {
-                --     relative_time = false,
-                -- },
-                -- sign_priority = 6,
+                sign_priority = 6,
                 update_debounce = 200,
                 max_file_length = 40000, -- Disable if file is longer than this (in lines)
                 preview_config = {
@@ -91,7 +97,7 @@ return {
             )
 
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>")
-            vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>")
+            vim.keymap.set("n", "<leader>gd", ":Gitsigns diffthis HEAD<CR>")
 
         end
     }
