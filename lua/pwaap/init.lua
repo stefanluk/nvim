b933dd8aa3866ff34368cf161eb7d8af7eafaff7
Stefan Luk <sluk@anwb.nl>
2024-04-22 15:08:41 +0200

    keymap update

--- lua/pwaap/init.lua
+++ lua/pwaap/init.lua
@@ -6,10 +6,23 @@ local augroup = vim.api.nvim_create_augroup
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
+local PwaapGroup = augroup('Pwaap', {})
 
 autocmd('TextYankPost', {
     group = yank_group,
     callback = function()
         vim.highlight.on_yank()
     end,
+})
+
+autocmd('LspAttach', {
+    group = PwaapGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
 })
--- lua/pwaap/remap.lua
+++ lua/pwaap/remap.lua
@@ -4,11 +4,13 @@ vim.g.mapleader = " "
 -- NORMAL MODE
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
 -- Clear highlight search on pressing <Esc> in normal mode
 
 -- Move text up and down
-vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<cr>")
-vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<cr>")
+vim.keymap.set("n", "<A-k>", "<Esc>:m .-2<Cr>")
+vim.keymap.set("n", "<A-j>", "<Esc>:m .+1<Cr>")
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
@@ -16,8 +18,13 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Navigate panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
--- INSERT MODE 
+-- INSERT MODE
 vim.keymap.set("i", "jj", "<ESC>")
 
 
@@ -28,5 +35,3 @@ vim.keymap.set("v", ">", ">gv")
 -- Move text up and down
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
-
-
